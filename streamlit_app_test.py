da37931ba59b839a4dd03964c65cff152559f9e7
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-01-24 14:57:36 +0100

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -16,6 +16,36 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+def primarychoice():
+    st.sidebar.info("This app is maintained by Michoux Julien. " "You can contact me at [ju.michoux@gmail.com].")
+    st.sidebar.title("Let's choose your asset type")
+    l_assettype = ['Future /Forward', 'Equity', 'other']
+    l_assettypechoice = st.sidebar.radio("Choose your asset type : ", l_assettype)
+
+    if l_assettypechoice == 'Future /Forward':
+        forward_future()
+        
+def possibilities_via_dictionnary(p_question):
+    l_question = p_question
+    data = {'Question_1': ['Choose','VRAI','FAUX'],\
+        'Question_2': ['Choose','VRAI','FAUX'],\
+        'Question_3': ['Choose','VRAI','FAUX'], \
+        'Question_4': ['Choose','VRAI','FAUX'],\
+        'Question_5': ['Choose','VRAI','FAUX'], \
+        'Question_6': ['Choose','VRAI','FAUX'],\
+        'Question_7': ['Choose','VRAI','FAUX'],\
+        'Question_8': ['Choose','VRAI','FAUX'],\
+        'Question_9': ['Choose','VRAI','FAUX'],\
+        'Question_10': ['Choose','VRAI','FAUX'],\
+        'Question_11': ['Choose','A=1 & B=2','A=2 & B=1', ],\
+        'Question_12': ['Choose','VRAI','FAUX'],\
+        'Question_13': ['Choose','VRAI','FAUX'],\
+
+        }
+    l_possibiitiesforonequestion = (data[f'{l_question}'])
+    return(l_possibiitiesforonequestion)
+
 def forward_future():
     st.write("Un contrat future est un accord entre deux parties, l'une acheteuse (ou long), l'autre vendeuse (ou short), souhaitant s’échanger un sous-jacent à une date future, à un prix fixé à l’avance.")
     st.write("Les origines des contrats à terme sont anciennes. Si, dès la Mésopotamie ancienne, certaines formes d’accords circulaient déjà, notamment grâce au Code de Hammurabi, il faudra néanmoins attendre le milieu du XIXème siècle pour voir émerger une Bourse dédiée et organisée, aux Etats-Unis, avec le Chicago Board of Trade (CBOT). \
