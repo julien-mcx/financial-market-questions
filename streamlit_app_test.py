eb84bdc6d796d85cf20cf94f40c32fb390efcb82
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-01-24 14:29:49 +0100

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import 
 
 """
 # Welcome to Streamlit!
@@ -14,25 +15,13 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+def forward_future():
+    st.write("Un contrat future est un accord entre deux parties, l'une acheteuse (ou long), l'autre vendeuse (ou short), souhaitant s’échanger un sous-jacent à une date future, à un prix fixé à l’avance.")
+    st.write("Les origines des contrats à terme sont anciennes. Si, dès la Mésopotamie ancienne, certaines formes d’accords circulaient déjà, notamment grâce au Code de Hammurabi, il faudra néanmoins attendre le milieu du XIXème siècle pour voir émerger une Bourse dédiée et organisée, aux Etats-Unis, avec le Chicago Board of Trade (CBOT). \
+        Un premier contrat forward, sur le blé, est ainsi signé le 13 mars 1851. Quelques années plus tard, en 1865, les contrats futures standardisés font leur apparition.")
+    st.write("Un contrat forward est un accord d'achat ou de vente d'un actif (appelé sous-jacent) à une date future T (appelée la maturité) pour un certain prix (appelé le prix d'exercice).")
+    st.write("Un éventail large de matières premières et actifs financiers forment ce que l'on appelle actif sous-jacent")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == '__main__':
+    forward_future()
