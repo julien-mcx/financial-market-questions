e749e51dd1117902cba40b0cf72148f0aa003da4
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-01-31 17:13:08 +0100

    Create streamlit_app_test2.py

---
+++ streamlit_app_test2.py
@@ -0,0 +1,33 @@
+# Streamlit-Google Sheet
+## Modules
+import streamlit as st 
+from pandas import DataFrame
+
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
+
+# Application Related Module
+import pubchempy as pcp
+from pysmiles import read_smiles
+# 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+
+# Disable certificate verification (Not necessary always)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "Database"
+spread = Spread(spreadsheetname,client = client)
+
+# Check the connection
+st.write(spread.url)
