f70ce2eff750533c218685b4266562a5a6391897
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-01 10:00:32 +0100

    Update streamlit_app_test2.py

--- streamlit_app_test2.py
+++ streamlit_app_test2.py
@@ -11,12 +11,12 @@ conn = connect()
 # # Perform SQL query on the Google Sheet.
 # # Uses st.cache to only rerun when the query changes or after 10 min.
 # @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
-# sheet_url = st.secrets["public_gsheets_url"]
+sheet_url = st.secrets["public_gsheets_url"]
 # rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # # Print results.
