a430e67724a5d3bc9f6953005fd25b52b3ab3d3c
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-01 10:13:02 +0100

    Update streamlit_app_test2.py

--- streamlit_app_test2.py
+++ streamlit_app_test2.py
@@ -17,6 +17,9 @@ def run_query(query):
     return rows
 
 sheet_url = st.secrets["public_gsheets_url"]
+st.write(sheet_url)
+st.write(conn)
+
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # # Print results.
