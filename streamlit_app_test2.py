fa2a0bff916070a0e699497b2553f351a5b8a1af
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-02-01 09:59:12 +0100

    Update streamlit_app_test2.py

--- streamlit_app_test2.py
+++ streamlit_app_test2.py
@@ -3,20 +3,22 @@
 import streamlit as st
 from gsheetsdb import connect
 
-# Create a connection object.
-conn = connect()
+st.write("gg")
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+# # Create a connection object.
+# conn = connect()
 
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# # Perform SQL query on the Google Sheet.
+# # Uses st.cache to only rerun when the query changes or after 10 min.
+# @st.cache(ttl=600)
+# def run_query(query):
+#     rows = conn.execute(query, headers=1)
+#     rows = rows.fetchall()
+#     return rows
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+# sheet_url = st.secrets["public_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# # Print results.
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
