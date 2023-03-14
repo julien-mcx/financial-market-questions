26733ec431a65335ee9ac6e751647f63a44669d2
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-03-14 19:49:10 +0100

    Update test33.py

--- test33.py
+++ test33.py
@@ -1,5 +1,4 @@
 import pandas as pd
 #For Excel File
-df = pd.read_excel("world-happiness-report-2021.xlxs")
-#For CSV File
-df = pd.read_csv("world-happiness-report-2021.csv")
+df = pd.read_excel("QuestionsFinancedeMarche.xlsx")
+
