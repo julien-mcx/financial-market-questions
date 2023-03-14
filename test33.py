1834f59c507ae5c7e5217001c6ce0ba508ce9fa0
julien-mcx <123368888+julien-mcx@users.noreply.github.com>
2023-03-14 19:48:35 +0100

    Update test33.py

--- test33.py
+++ test33.py
@@ -1,4 +1,5 @@
-from st_aggrid import AgGrid
-
-df = pd.read_excel("QuestionsFinancedeMarche.xlsx")
-AgGrid(df)
+import pandas as pd
+#For Excel File
+df = pd.read_excel("world-happiness-report-2021.xlxs")
+#For CSV File
+df = pd.read_csv("world-happiness-report-2021.csv")
